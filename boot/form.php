9519dbd0b6c701c9064e0f3899a28cd242c4ad1b
nassbite <127749243+nassbite@users.noreply.github.com>
2023-03-13 15:15:06 +0300

    Add initial nasslovebite website files

---
+++ boot/form.php
@@ -0,0 +1,9 @@
+<?php   
+
+echo 'Your email is ' . $_POST['Email']  . '<br />';
+echo  'Your name is ' . $_POST['name'] . '<br />';
+echo  'Your phone is ' . $_POST['Number'] . '<br />';
+echo 'Your messsage is '. $_POST['comment'] .'<br />';
+echo  'Your gender is' . $_POST['gender'] .'<br />';
+
+?>
